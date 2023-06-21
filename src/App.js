64dcac8ec0628c400af105d5b4b6df037371ef92
Aswad Tariq <aswadtariq05@gmail.com>
2023-06-21 15:14:38 +0500

    Added initial card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './components/card/card.jsx';
+import './components/card-list/card-list.styles.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.jsx';
+
+import './App.css';
+
+
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state={
+        monsters:[
+          {
+            name: 'Frankenstein',
+            email: 'frank@hotmail.com',
+            id: '1'
+          },
+          {
+            name: 'Dracula',
+            email:'drac@gmail.com',
+            id: '2'
+          },
+          {
+            name: 'Zombie',
+            email: 'zomb@knowledgetech.com',
+            id: '3'
+          },
+        ],
+      }
+}
+render() {
+    
+    return (
+      <div className='App'>
+        <h1>Monsters Rolodex</h1>
+        <div className='card-list'>
+          {this.state.monsters.map(monster => (
+            <Card props={monster}/>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
